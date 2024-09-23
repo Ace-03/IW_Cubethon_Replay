5e92f1de45ec4641110a9e74386dca476737cfdd
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2024-09-22 20:01:48 -0700

    added replay

--- Cubethon Replay Build/Assets/Scripts/GameManager.cs
+++ Cubethon Replay Build/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,45 @@ public class GameManager : MonoBehaviour
     bool gameHasEnded = false;
     public float restartDelay = 1f;
     public GameObject completeLevelUI;
+    public GameObject replayUI;
+    public GameObject player;
+    public bool instantReplay;
+
+    float replayStartTime;
+
+    void Start()
+    {
+        replayUI.SetActive(false);
+        if (CommandLog.commands1.Count > 0)
+        {
+            instantReplay = true;
+            replayStartTime = Time.timeSinceLevelLoad;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (instantReplay)
+            RunInstantReplay();
+    }
+
+    private void RunInstantReplay()
+    {
+        if (CommandLog.commands1.Count == 0)
+            return;
+
+        replayUI.SetActive(true);
+        Command command = CommandLog.commands1.Peek();
+        if (Time.timeSinceLevelLoad >= command.timeStamp)
+        { 
+            command = CommandLog.commands1.Dequeue();
+            command.playerBody = player.GetComponent<Rigidbody>();
+            Invoker invoker = new Invoker();
+            invoker.disableLog = true;
+            //invoker.Setcommand(command);
+            invoker.ExcuteCommand();
+        }
+    }
 
     public void CompleteLevel()
     {
@@ -17,7 +57,7 @@ public class GameManager : MonoBehaviour
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
-            Debug.Log("Game Over");
+            //Debug.Log("Game Over");
             Invoke("Restart", restartDelay);
         }
     }
--- Cubethon Replay Build/Assets/Scripts/PlayerMovement.cs
+++ Cubethon Replay Build/Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,11 @@ public class PlayerMovement : MonoBehaviour
     public float fowardforce = 2000f;
     public float sidewaysforce = 100f;
 
+    GameManager manager;
+
     void Start()
     {
-        
+       manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -20,14 +22,26 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.AddForce(0, 0, fowardforce * Time.deltaTime);
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") && !manager.instantReplay)
         {
-            rb.AddForce(sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            //rb.AddForce(sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            //Take inputs and send them to command log
+            Command moveRight = new MoveRight(rb, sidewaysforce);
+            Invoker invoker = new Invoker();
+            invoker.Setcommand(moveRight);
+            invoker.ExcuteCommand();
+            Debug.Log("Input : Right");
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") && !manager.instantReplay)
         {
-            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            //rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            //Take inputs and send them to command log
+            Command moveLeft = new MoveLeft(rb, sidewaysforce);
+            Invoker invoker = new Invoker();
+            invoker.Setcommand(moveLeft); 
+            invoker.ExcuteCommand();
+            Debug.Log("Input : Left");
         }
 
         if (rb.position.y < -2f)
